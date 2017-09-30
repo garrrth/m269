7bac5363d2b0caffdecec3aaf4f2771ffe27ed6a
Gareth Dackevych-Thomas <gwrath.thomas@gmail.com>
2017-09-30 02:04:08 +0100

    Chapter 1 exercises

--- Chapter1/fraction.py
+++ Chapter1/fraction.py
@@ -2,8 +2,20 @@ class Fraction:
 
     def __init__(self, top, bottom):
         
-        self.num = top
-        self.den = bottom
+        if not isinstance(top, int):
+            raise TypeError(str(top)+" is not of type Integer")
+
+        if not isinstance(bottom, int):
+            raise TypeError(str(bottom)+" is not of type Integer")
+
+        if bottom < 0:
+            top = top*-1
+            bottom = bottom*-1
+
+        common = gcd(top, bottom)
+
+        self.num = top//common
+        self.den = bottom//common
 
     def __str__(self):
 
@@ -11,19 +23,123 @@ class Fraction:
 
     def __add__(self, otherfraction):
 
+        if isinstance(otherfraction, int):
+            otherfraction = intToFraction(otherfraction)
+
         newnum = self.num*otherfraction.den + \
                     self.den*otherfraction.num
         newden = self.den * otherfraction.den
-        common = gcd(newnum, newden)
 
-        return Fraction(newnum//common, newden//common)
+        return Fraction(newnum, newden)
+
+    def __radd__(self, otherfraction):
+
+        if isinstance(otherfraction, int):
+            otherfraction = intToFraction(otherfraction)
+
+        newnum = self.num*otherfraction.den + \
+                    self.den*otherfraction.num
+        newden = self.den * otherfraction.den
+
+        return Fraction(newnum, newden)
+
+    def __sub__(self, otherfraction):
+
+        if isinstance(otherfraction, int):
+            otherfraction = intToFraction(otherfraction)
+
+        newnum = self.num * otherfraction.den - \
+                self.den * otherfraction.num
+
+        newden = self.den * otherfraction.den
+
+        return Fraction(newnum, newden)
+
+    def __mul__(self, otherfraction):
+
+        if isinstance(otherfraction, int):
+            otherfraction = intToFraction(otherfraction)
+
+        newnum = self.num * otherfraction.num
+        newden = self.den * otherfraction.den
+
+        return Fraction(newnum, newden)
+
+    def __truediv__(self, otherfraction):
+
+        if isinstance(otherfraction, int):
+            otherfraction = intToFraction(otherfraction)
+
+        newnum = self.num * otherfraction.den
+        newden = self.den * otherfraction.num
+
+        return Fraction(newnum, newden)
 
     def __eq__(self, otherfraction):
+
+        if isinstance(otherfraction, int):
+            otherfraction = intToFraction(otherfraction)
 
         firstnum = self.num * otherfraction.den
         secondnum = self.den * otherfraction.num
 
         return firstnum == secondnum
+
+    def __gt__(self, otherfraction):
+
+        if isinstance(otherfraction, int):
+            otherfraction = intToFraction(otherfraction)
+
+        firstnum = self.num * otherfraction.den
+        secondnum = self.den * otherfraction.num
+
+        return firstnum > secondnum
+
+    def __lt__(self, otherfraction):
+
+        if isinstance(otherfraction, int):
+            otherfraction = intToFraction(otherfraction)
+
+        firstnum = self.num * otherfraction.den
+        secondnum = self.den * otherfraction.num
+
+        return firstnum < secondnum
+
+    def __ge__(self, otherfraction):
+
+        if isinstance(otherfraction, int):
+            otherfraction = intToFraction(otherfraction)
+
+        firstnum = self.num * otherfraction.den
+        secondnum = self.den * otherfraction.num
+
+        return firstnum >= secondnum
+
+    def __le__(self, otherfraction):
+
+        if isinstance(otherfraction, int):
+            otherfraction = intToFraction(otherfraction)
+
+        firstnum = self.num * otherfraction.den
+        secondnum = self.den * otherfraction.num
+
+        return firstnum <= secondnum
+
+    def __ne__(self, otherfraction):
+
+        if isinstance(otherfraction, int):
+            otherfraction = intToFraction(otherfraction)
+
+        firstnum = self.num * otherfraction.den
+        secondnum = self.den * otherfraction.num
+
+        return firstnum != secondnum
+
+    def getNum(self):
+        return self.num
+
+    def getDen(self):
+        return self.den
 
 # HELPER METHODS #
 
@@ -37,3 +153,6 @@ def gcd(m,n):
         n = oldm%oldn
 
     return n
+
+def intToFraction(n):
+    return Fraction(n, 1)
